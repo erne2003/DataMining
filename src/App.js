602155928171d23a0c00de12400b2f1cb1322415
Ernesto Cardoso <ebarreirojunior@gmail.com>
2025-07-15 13:51:05 -0400

    Local host up and running Bootstrap icons installed

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Fresh Market
+      </header>
+      
+      <h2 className="Classification-NavBar">
+
+<button className='Category-Button'>Grocery   <i class="bi bi-arrow-down"style={{position:"relative", top:"2px", height:"2px"}}></i></button>
+<button className='Category-Button'>Produce <i class="bi bi-arrow-down"style={{position:"relative", top:"2px", height:"2px"}}></i></button>
+<button className='Category-Button'>Bakery <i class="bi bi-arrow-down"style={{position:"relative", top:"2px", height:"2px"}}></i></button>
+<button className='Category-Button'>Dairy & Eggs <i class="bi bi-arrow-down"style={{position:"relative", top:"2px", height:"2px"}}></i></button>
+<button className='Category-Button'>Meat & Seafood <i class="bi bi-arrow-down"style={{position:"relative", top:"2px", height:"2px"}}></i></button>
+<button className='Category-Button'>Frozen Foods <i class="bi bi-arrow-down"style={{position:"relative", top:"2px", height:"2px"}}></i></button>
+<button className='Category-Button'>Beverages <i class="bi bi-arrow-down"style={{position:"relative", top:"2px", height:"2px"}}></i></button>
+<button className='Category-Button'>Snacks & Sweets <i class="bi bi-arrow-down"style={{position:"relative", top:"2px", height:"2px"}}></i></button>
+<button className='Category-Button'>Household Essentials <i class="bi bi-arrow-down"style={{position:"relative", top:"2px", height:"2px"}}></i></button>
+<button className='Category-Button'>Health & Beauty <i class="bi bi-arrow-down"style={{position:"relative", top:"2px", height:"2px"}}></i></button>
+
+
+      </h2>
+    </div>
+  );
+}
+
+export default App;
