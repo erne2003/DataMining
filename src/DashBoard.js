f8aaaf197db027d5af4f43f422ab833a71e77ed3
Ernesto C <ebarreirojunior@gmail.com>
2025-07-18 12:54:07 -0400

    The dashboard is done, just need to implement the option to filter by category and work on storing transactions

--- src/DashBoard.js
+++ src/DashBoard.js
@@ -16,7 +16,7 @@ function DashBoard() {
 <button className='Category-Button'>Produce <i class="bi bi-arrow-down"style={{position:"relative", top:"2px", height:"2px"}}></i></button>
 <button className='Category-Button'>Bakery <i class="bi bi-arrow-down"style={{position:"relative", top:"2px", height:"2px"}}></i></button>
 <button className='Category-Button'>Dairy & Eggs <i class="bi bi-arrow-down"style={{position:"relative", top:"2px", height:"2px"}}></i></button>
-<button className='Category-Button'>Meat & Seafood <i class="bi bi-arrow-down"style={{position:"relative", top:"2px", height:"2px"}}></i></button>
+<button className='Category-Button'>Meat & Seafood <i class="bi bi-arrow-down"style={{position:"relative", top:"2px", height:"2px", }}></i></button>
 <button className='Category-Button'>Frozen Foods <i class="bi bi-arrow-down"style={{position:"relative", top:"2px", height:"2px"}}></i></button>
 <button className='Category-Button'>Beverages <i class="bi bi-arrow-down"style={{position:"relative", top:"2px", height:"2px"}}></i></button>
 <button className='Category-Button'>Snacks & Sweets <i class="bi bi-arrow-down"style={{position:"relative", top:"2px", height:"2px"}}></i></button>
@@ -25,106 +25,110 @@ function DashBoard() {
       </h2>
 
 
-      <div className='Items-Container'>
+<div className='Items-Container'>
+<button style={{marginLeft:"20px", marginRight:"20px"}}>
 <div className="Product-Image">
-<img src="/Assets/076326-600x600-A.avif" alt="Product Image" />
+<img src="/Assets/076326-600x600-A.avif" alt="Product Image" style={{position:"relative", right:"7px"}} />
 <p style={{position:"relative", top:"105px", right:"95px", fontSize:"12px", width:"200px"}}>Vanilla Ice Cream</p>
 </div>
+</button>
 
-<div className="Product-Image">
-<img src="/Assets/Clorox Disinfecting Wipes.jpg" alt="Product Image" />
+<button style={{marginLeft:"20px", marginRight:"20px"}}><div className="Product-Image">
+<img src="/Assets/Clorox Disinfecting Wipes.jpg" alt="Product Image" style={{position:"relative", right:"7px"}} />
 <p style={{position:"relative", top:"105px", right:"95px", fontSize:"12px", width:"200px"}}>Vanilla Ice Cream</p>
-</div>
+</div></button>
 
-<div className="Product-Image">
-<img src="/Assets/Colgate Total Toothpaste.avif" alt="Product Image" />
+<button style={{marginLeft:"20px", marginRight:"20px"}}><div className="Product-Image">
+<img src="/Assets/Colgate Total Toothpaste.avif" alt="Product Image" style={{position:"relative", right:"6px"}}/>
 <p style={{position:"relative", top:"105px", right:"95px", fontSize:"12px", width:"200px"}}>Vanilla Ice Cream</p>
-</div>
+</div></button>
 
-<div className="Product-Image">
-<img src="/Assets/Dove Sensitive Skin Body Wash.avif" alt="Product Image" />
+<button style={{marginLeft:"20px", marginRight:"20px"}}><div className="Product-Image">
+<img src="/Assets/Dove Sensitive Skin Body Wash.avif" alt="Product Image" style={{position:"relative", right:"6px"}}/>
 <p style={{position:"relative", top:"105px", right:"95px", fontSize:"12px", width:"200px"}}>Vanilla Ice Cream</p>
-</div>
+</div></button>
 
-<div className="Product-Image">
-<img src="/Assets/Fresh Strawberries.avif" alt="Product Image" />
+<button style={{marginLeft:"20px", marginRight:"20px"}}><div className="Product-Image">
+<img src="/Assets/Fresh Strawberries.avif" alt="Product Image" style={{position:"relative", right:"6px"}}/>
 <p style={{position:"relative", top:"105px", right:"95px", fontSize:"12px", width:"200px"}}>Vanilla Ice Cream</p>
-</div>
+</div></button>
 
-<div className="Product-Image">
-<img src="/Assets/Organic Avocados.avif" alt="Product Image" />
+<button style={{marginLeft:"20px", marginRight:"20px"}}><div className="Product-Image">
+<img src="/Assets/Organic Avocados.avif" alt="Product Image" style={{position:"relative", right:"6px"}}/>
 <p style={{position:"relative", top:"105px", right:"95px", fontSize:"12px", width:"200px"}}>Vanilla Ice Cream</p>
-</div>
+</div></button>
 
-<div className="Product-Image">
-<img src="/Assets/Paper Towel.avif" alt="Product Image" />
+<button style={{marginLeft:"20px", marginRight:"20px"}}><div className="Product-Image">
+<img src="/Assets/Paper Towel.avif" alt="Product Image" style={{position:"relative", right:"6px"}}/>
 <p style={{position:"relative", top:"105px", right:"95px", fontSize:"12px", width:"200px"}}>Vanilla Ice Cream</p>
-</div>
+</div></button>
 
-<div className="Product-Image">
-<img src="/Assets/Publix Bakery Chocolate Chip Cookies.jpg" alt="Product Image" />
+<button style={{marginLeft:"20px", marginRight:"20px"}}><div className="Product-Image">
+<img src="/Assets/Publix Bakery Chocolate Chip Cookies.jpg" alt="Product Image" style={{position:"relative", right:"6px"}} />
 <p style={{position:"relative", top:"105px", right:"95px", fontSize:"12px", width:"200px"}}>Vanilla Ice Cream</p>
 </div>
+</button>
 
-<div className="Product-Image">
-<img src="/Assets/Publix Bakery Sourdough Bread.avif" alt="Product Image" />
+<button style={{marginLeft:"20px", marginRight:"20px"}}><div className="Product-Image">
+<img src="/Assets/Publix Bakery Sourdough Bread.avif" alt="Product Image" style={{position:"relative", right:"6px"}}/>
 <p style={{position:"relative", top:"105px", right:"95px", fontSize:"12px", width:"200px"}}>Vanilla Ice Cream</p>
-</div>
+</div></button>
 
-<div className="Product-Image">
-<img src="/Assets/Publix Chicken Tenderloins.avif" alt="Product Image" />
+<button style={{marginLeft:"20px", marginRight:"20px"}}><div className="Product-Image">
+<img src="/Assets/Publix Chicken Tenderloins.avif" alt="Product Image" style={{position:"relative", right:"6px"}}/>
 <p style={{position:"relative", top:"105px", right:"95px", fontSize:"12px", width:"200px"}}>Vanilla Ice Cream</p>
-</div>
+</div></button>
 
-<div className="Product-Image">
-<img src="/Assets/Publix Chocolate Chip Cookies.avif" alt="Product Image" />
+<button style={{marginLeft:"20px", marginRight:"20px"}}><div className="Product-Image">
+<img src="/Assets/Publix Chocolate Chip Cookies.avif" alt="Product Image" style={{position:"relative", right:"6px"}}/>
 <p style={{position:"relative", top:"105px", right:"95px", fontSize:"12px", width:"200px"}}>Vanilla Ice Cream</p>
-</div>
+</div></button>
 
-<div className="Product-Image">
-<img src="/Assets/Publix Fresh Salmon Fillets.avif" alt="Product Image" />
+<button style={{marginLeft:"20px", marginRight:"20px"}}><div className="Product-Image">
+<img src="/Assets/Publix Fresh Salmon Fillets.avif" alt="Product Image" style={{position:"relative", right:"6px"}}/>
 <p style={{position:"relative", top:"105px", right:"95px", fontSize:"12px", width:"200px"}}>Vanilla Ice Cream</p>
-</div>
+</div></button>
 
-<div className="Product-Image">
-<img src="/Assets/Publix Frozen Broccoli Florets.avif" alt="Product Image" />
+<button style={{marginLeft:"20px", marginRight:"20px"}}><div className="Product-Image">
+<img src="/Assets/Publix Frozen Broccoli Florets.avif" alt="Product Image" style={{position:"relative", right:"6px"}}/>
 <p style={{position:"relative", top:"105px", right:"95px", fontSize:"12px", width:"200px"}}>Vanilla Ice Cream</p>
-</div>
+</div></button>
 
-<div className="Product-Image">
-<img src="/Assets/Publix Grade A Cage Free Eggs.avif" alt="Product Image" />
+<button style={{marginLeft:"20px", marginRight:"20px"}} ><div className="Product-Image">
+<img src="/Assets/Publix Grade A Cage Free Eggs.avif" alt="Product Image" style={{position:"relative", right:"6px"}}/>
 <p style={{position:"relative", top:"105px", right:"95px", fontSize:"12px", width:"200px"}}>Vanilla Ice Cream</p>
-</div>
+</div></button>
 
-<div className="Product-Image">
-<img src="/Assets/Publix GreenWise Organic Milk.avif" alt="Product Image" />
+<button style={{marginLeft:"20px", marginRight:"20px"}}><div className="Product-Image">
+<img src="/Assets/Publix GreenWise Organic Milk.avif" alt="Product Image"style={{position:"relative", right:"6px"}} />
 <p style={{position:"relative", top:"105px", right:"95px", fontSize:"12px", width:"200px"}}>Vanilla Ice Cream</p>
-</div>
+</div></button>
 
-<div className="Product-Image">
-<img src="/Assets/Publix GreenWise Organic Pasta.avif" alt="Product Image" />
+<button style={{marginLeft:"20px", marginRight:"20px"}}><div className="Product-Image">
+<img src="/Assets/Publix GreenWise Organic Pasta.avif" alt="Product Image" style={{position:"relative", right:"6px"}}/>
 <p style={{position:"relative", top:"105px", right:"95px", fontSize:"12px", width:"200px"}}>Vanilla Ice Cream</p>
-</div>
+</div></button>
 
-<div className="Product-Image">
-<img src="/Assets/Publix Pretzel Crisps.avif" alt="Product Image" />
+<button style={{marginLeft:"20px", marginRight:"20px"}}><div className="Product-Image">
+<img src="/Assets/Publix Pretzel Crisps.avif" alt="Product Image" style={{position:"relative", right:"6px"}}/>
 <p style={{position:"relative", top:"105px", right:"95px", fontSize:"12px", width:"200px"}}>Vanilla Ice Cream</p>
-</div>
+</div></button>
 
-<div className="Product-Image">
-<img src="/Assets/Stouffer's Frozen Lasagna.avif" alt="Product Image" />
+<button style={{marginLeft:"20px", marginRight:"20px"}}><div className="Product-Image">
+<img src="/Assets/Stouffer's Frozen Lasagna.avif" alt="Product Image" style={{position:"relative", right:"6px"}}/>
 <p style={{position:"relative", top:"105px", right:"95px", fontSize:"12px", width:"200px"}}>Vanilla Ice Cream</p>
-</div>
+</div></button>
 
-<div className="Product-Image">
-<img src="/Assets/Tropicana Light Orange Juice.avif" alt="Product Image" />
+<button style={{marginLeft:"20px", marginRight:"20px"}}><div className="Product-Image">
+<img src="/Assets/Tropicana Light Orange Juice.avif" alt="Product Image" style={{position:"relative", right:"6px"}}/>
 <p style={{position:"relative", top:"105px", right:"95px", fontSize:"12px", width:"200px"}}>Vanilla Ice Cream</p>
-</div>
+</div></button>
 
-<div className="Product-Image">
-<img src="/Assets/Vitaminwater Zero Sugar.avif" alt="Product Image" />
+<button style={{marginLeft:"20px", marginRight:"20px"}}><div className="Product-Image">
+<img src="/Assets/Vitaminwater Zero Sugar.avif" alt="Product Image" style={{position:"relative", right:"6px"}}/>
 <p style={{position:"relative", top:"105px", right:"95px", fontSize:"12px", width:"200px"}}>Vanilla Ice Cream</p>
-</div>
+</div></button>
+
 
       </div>
     </div>
