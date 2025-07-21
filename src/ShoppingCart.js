e9c3bd2c5dfaf741cf54679a3f1fc55510d299b3
Ernesto Cardoso <ebarreirojunior@gmail.com>
2025-07-21 13:23:46 -0400

    Nothing

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -10,7 +10,7 @@ function ShoppingCart() {
 
   return (
     <div className="ShoppingCart">
-      <h2 style={{ position: "relative", left: "500px" }}>
+      <h2 style={{ position: "relative", left: "500px", top:"40px" }}>
         Your Shopping Cart
       </h2>
 
@@ -39,9 +39,9 @@ function ShoppingCart() {
         top: "40px",
         fontWeight: "bold"
       }}>
-        <div>Item</div>
-        <div>Quantity</div>
-        <div>Total</div>
+        <div style={{position:"relative", top:"30px"}}>Item</div>
+        <div style={{position:"relative", left:"50px",top:"30px"}}>Quantity</div>
+        <div style={{position:"relative", left:"17px",top:"30px"}}>Total</div>
       </div>
 
       {/* Cart items */}
@@ -49,7 +49,7 @@ function ShoppingCart() {
         className="Transaction-Body"
         style={{
           position: "relative",
-          left: "480px",
+          left: "450px",
           top: "80px",
           border: "1px solid black",
           padding: "10px",
@@ -64,7 +64,9 @@ function ShoppingCart() {
               style={{
                 display: "flex",
                 alignItems: "center",
-                marginBottom: "8px"
+                marginBottom: "8px",
+                width:"350px",
+                gap:"10px"
               }}
             >
               <img
@@ -73,18 +75,19 @@ function ShoppingCart() {
                 style={{ width: "30px", height: "30px", marginRight: "10px" }}
               />
               <span style={{ flex: 1 }}>{item.name}</span>
-              <span style={{ width: "50px", textAlign: "center" }}>
+              <span style={{ width: "50px", textAlign: "center",position:"relative", left:"-5px"}}>
                 {item.quantity}
               </span>
-              <span style={{ width: "70px", textAlign: "right" }}>
+              <span style={{ width: "70px", textAlign: "right",position:"relative", left:"25px"}}>
                 ${(item.price * item.quantity).toFixed(2)}
               </span>
             </div>
           ))
         ) : (
-          <p>Your cart is empty.</p>
+          <p style={{display:"flex", justifyContent:"center", marginTop:"12px"}}>Your cart is empty.</p>
         )}
       </div>
+    <button className="Payment-Button" style={{position:"relative", left:"600px", top:"200px", width:"100px"}}>Pay</button>
     </div>
   );
 }
